fa54f212954c954c1485e71b32689a2d3f773b38
Pudding Chen <pulipuli.chen@gmail.com>
2014-08-08 23:20:24 +0800

    0808 開始進行初期的修正

---
+++ proxmox-ve/pve/scripts/pve-cluster_activity_unblocked.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# 
+# 解除Proxmox VE Cluster被鎖定狀態
+# 
+
+base_dir=${PWD}
+
+is_blocked=`$base_dir/cluster_is_activity_blocked.sh`
+
+if [ $is_blocked == 1 ];then
+    pvecm expected 1
+    service pve-cluster restart
+
+    if [ -f $base_dir/mailtoa.sh ];then
+        $base_dir/mailto.sh "cluster_activity_unblocked"
+    fi
+fi
--- proxmox-ve/pve/scripts/pve-decluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-service cman stop
-killall -9 corosync cman dlm_controld fenced
-service pve-cluster stop
-rm /etc/cluster/cluster.conf
-rm -rf /var/lib/pve-cluster/* /var/lib/pve-cluster/.*
-rm /var/lib/cluster/*
-reboot
